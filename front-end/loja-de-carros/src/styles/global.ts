3657aa129a2d77d52478d397ba561cb3a395ffee
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-18 13:30:25 -0300

    Feat: created style global

---
+++ front-end/loja-de-carros/src/styles/global.ts
@@ -0,0 +1,185 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+:root{
+ --color-brand-1 : #4529e6;
+ --color-brand-2 : #5126ea;
+ --color-brand-3 : #b0a6f0;
+ --color-brand-4 : #edeafd;
+
+ --color-grey-0 : #0b0d0d;
+ --color-grey-1 : #212529;
+ --color-grey-2 : #495057;
+ --color-grey-3 : #868e96;
+ --color-grey-4 : #adb5bd;
+ --color-grey-5 : #ced4da;
+ --color-grey-6 : #dee2e6;
+ --color-grey-7 : #e9ecef;
+ --color-grey-8 : #f1f3f5;
+ --color-grey-9 : #f8f9fa;
+ --color-grey-10 : #fdfdfd;
+ --white-fixed : #FFFFFF;
+
+ --color-alert-1 : #cd2b31;
+ --color-alert-2 : #fdd8d8;
+ --color-alert-3 : #ffe5e5;
+ --color-sucess-1 : #18794e;
+ --color-sucess-2 : #ccebd7;
+ --color-sucess-3 : #ddf3e4;
+
+ --color-random-1 : #e34d8c;
+ --color-random-2 : #c04277;
+ --color-random-3 : #7d2a4d;
+ --color-random-4 : #7000ff;
+ --color-random-5 : #6200e3;
+ --color-random-6 : #36007d;
+ --color-random-7 : #349974;
+ --color-random-8 : #2a7d5f;
+ --color-random-9 : #153d2e;
+ --color-random-10 : #6100ff;
+ --color-random-11 : #5700e3;
+ --color-random-12 : #30007d;
+
+
+ --heading-1-700 : {
+    font-size: 44px;
+    font-weight:700;
+    color: var(--color-grey-1)
+
+ }
+ --heading-2-600 : {
+    font-size: 36px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-3-600 : {
+    font-size: 32px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-3-500 : {
+    font-size: 32px;
+    font-weight:500;
+    color: var(--color-grey-1)
+
+ }
+ --heading-4-600 : {
+    font-size: 28px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-4-500 : {
+    font-size: 28px;
+    font-weight:500;
+    color: var(--color-grey-1)
+
+ }
+ --heading-5-600 : {
+    font-size: 24px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-5-500 : {
+    font-size: 24px;
+    font-weight:500;
+    color: var(--color-grey-1)
+
+ }
+ --heading-6-600 : {
+    font-size: 20px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-6-500 : {
+    font-size: 20px;
+    font-weight:500;
+    color: var(--color-grey-1)
+
+ }
+ --heading-7-600 : {
+    font-size: 16px;
+    font-weight:600;
+    color: var(--color-grey-1)
+
+ }
+ --heading-7-500 : {
+    font-size: 16px;
+    font-weight:500;
+    color: var(--color-grey-1)
+
+ }
+
+ --body-1-400 : {
+    font-size: 16px;
+    font-weight:400;
+    color: var(--color-grey-2)
+ }
+ --body-1-600 : {
+    font-size: 16px;
+    font-weight:600;
+    color: var(--color-grey-2)
+ }
+ --body-2-400 : {
+    font-size: 14px;
+    font-weight:400;
+    color: var(--color-grey-2)
+ }
+ --body-2-500 : {
+    font-size: 14px;
+    font-weight:500;
+    color: var(--color-grey-2)
+ }
+ --button-big-text : {
+    font-size: 16px;
+    font-weight:600;
+    color: var(--color-grey-2)
+ }
+ --button-medium-text : {
+    font-size: 14px;
+    font-weight:600;
+    color: var(--color-grey-2)
+ }
+ --input-placeholder : {
+    font-size: 16px;
+    font-weight:400;
+    color: var(--color-grey-2)
+ }
+ --input-label :{
+    font-size: 14px;
+    font-weight:500;
+    color: var(--color-grey-2)
+ }
+
+
+}
+
+
+body {
+    font-family: 'Inter', sans-serif;
+    }
+
+    h1,h2,h3{
+        font-family: 'Inter', sans-serif;
+    }
+
+    p,span{
+        font-family: 'Inter', sans-serif;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+* {
+    margin:0;
+    padding: 0;
+    outline:0;
+    box-sizing: border-box;
+    }
+
+`;
