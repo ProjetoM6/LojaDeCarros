2f3af493e8a444e2e9967dfa3c4ae3c80a99ed26
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-23 10:38:42 -0300

    Feat: <main/> page user

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,27 @@
 import CardAuction from "./components/CarAuction";
 import AppHeader from "./components/Header";
 import Global from "./styles/global";
+import "./style.css"
+import Button from "./components/Button/style";
 
 function App() {
   return (
     <>
       <Global />
       <AppHeader />
-      <CardAuction />
+      {/* <CardAuction /> */}
+      <main>
+        <div className="azul">a</div>
+        <section>
+          <img src="" alt="" />
+          <div>
+            <h2>User usuario</h2> 
+            <span>tipo de user</span>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <Button background="var(--color-grey-10)" borderColor="var(--color-brand-1)" color="var(--color-brand-1)" width="larger" hover="">Criar Anuncio</Button>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
