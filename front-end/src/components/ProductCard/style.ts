ea359555257cc81559d994bd554e0b0df4f1ab22
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-25 01:00:28 -0300

    Fix: max-width to min-width 300px

--- front-end/src/components/ProductCard/style.ts
+++ front-end/src/components/ProductCard/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const ProductCardStyled = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 300px;
+  min-width: 300px;
   gap: 15px;
   position: relative;
   &:hover {
