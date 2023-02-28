dc9c65a9ebca2e8d033fb1e146aebbcbe9f89d75
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-28 16:12:34 -0300

    Feat: app.use router login

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import "reflect-metadata";
 import userRoutes from "./routes/user/user.routes";
+import loginRoutes from "./routes/login/login.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/user", userRoutes);
+app.use("/user/login", loginRoutes);
 
 app.listen(3331, () => {
   console.log("Server running");
