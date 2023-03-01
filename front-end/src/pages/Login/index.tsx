f02c7f6011ba5fa5bde5072608f2eea39ab7f8f2
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-01 18:38:32 -0300

    Refactor: Page componentization and styling

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -4,63 +4,14 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import AppHeader from "../../components/Header";
 import Footer from "../../components/Footer";
 import { LoginPage } from "./style";
+import FormLogin from "../../components/Forms/FormLogin";
 
 const Login = () => {
-  const formSchema = yup.object().shape({
-    email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
-    password: yup
-      .string()
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\.*])(?=.{8,})/,
-        "A senha deve conter 8 caraceteres, uma maiúscula, uma minúscula, um número e um caracter especial"
-      )
-      .required("Senha obrigatória"),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(formSchema),
-  });
-
   return (
     <LoginPage>
       <AppHeader />
-      <div className="geral">
-        <div className="container">
-          <h1 className="heading-5-500">Login</h1>
-          <form className="form">
-            <div className="info">
-              <label>Usuário</label>
-              <input
-                type="text"
-                placeholder="Digitar usuário"
-                {...register("email")}
-              />
-              {/* {errors.email && <span>{errors.email.message}</span>} */}
-
-              <label>Senha</label>
-              <input
-                type="text"
-                placeholder="Digitar senha"
-                {...register("senha")}
-              />
-              {/* {errors.senha && <span>{errors.senha.message}</span>} */}
-            </div>
-
-            <a href="#">Esqueci minha senha</a>
-
-            <div className="buttonLogin">
-              <button type="submit">Entrar</button>
-            </div>
-            <p>Ainda não possui conta?</p>
-            <div className="buttonRegister">
-              <button>Cadastrar</button>
-            </div>
-          </form>
-        </div>
+      <div className="container">
+        <FormLogin />
       </div>
       <Footer />
     </LoginPage>
