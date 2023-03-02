9b48bb7ebff4710aad64f983f8c27bfbd66aef56
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-02 19:05:28 -0300

    Feat:add  <AuthProvider/>

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,13 @@
+import { AuthProvider } from "./context/AuthContext";
 import RoutesMain from "./routes/Router";
 import Global from "./styles/global";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Global />
       <RoutesMain />
-    </>
+    </AuthProvider>
   );
 }
 
