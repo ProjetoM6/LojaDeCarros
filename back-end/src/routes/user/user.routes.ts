47af0ef3637d583a95479d16511b726ab4a1910b
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-02 22:56:27 -0300

    FIx: rename route /user/user to /user/profile

--- back-end/src/routes/user/user.routes.ts
+++ back-end/src/routes/user/user.routes.ts
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post("", createUserController);
 router.get("", listUsersController);
-router.get("/user", authUser, getOwnUserController);
+router.get("/profile", authUser, getOwnUserController);
 router.patch("/:id", authUser, updateUserController);
 router.delete("/:id", authUser, userDeleteController);
 export default router;
