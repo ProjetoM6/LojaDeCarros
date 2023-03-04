b1b69b857cd4e764edf0a373aa454cb9477fdbda
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-04 11:17:48 -0300

    Feat: add authContext in component

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,12 +1,13 @@
-import { useForm } from "react-hook-form";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 import AppHeader from "../../components/Header";
 import Footer from "../../components/Footer";
 import { LoginPage } from "./style";
 import FormLogin from "../../components/Forms/FormLogin";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 const Login = () => {
+  const { user } = useContext(AuthContext);
   return (
     <LoginPage>
       <AppHeader />
@@ -15,7 +16,17 @@ const Login = () => {
       </div>
       <Footer />
     </LoginPage>
-  );
+  ); /* user ? (
+    <Navigate to="/profile" replace />
+  ) : (
+    <LoginPage>
+      <AppHeader />
+      <div className="container">
+        <FormLogin />
+      </div>
+      <Footer />
+    </LoginPage>
+  ); */
 };
 
 export default Login;
--- front-end/src/pages/Register/index.tsx
+++ front-end/src/pages/Register/index.tsx
@@ -2,9 +2,15 @@ import AppHeader from "../../components/Header";
 import Footer from "../../components/Footer";
 import { RegisterPage } from "./style";
 import FormRegister from "../../components/Forms/FormRegister";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 const Register = () => {
-  return (
+  const { user } = useContext(AuthContext);
+  return user ? (
+    <Navigate to="/profile" replace />
+  ) : (
     <RegisterPage>
       <AppHeader />
       <div className="container">
