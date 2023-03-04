c50cdc21b69a85ed771ac433e295449c6f4496ad
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-04 17:06:40 -0300

    Feat: create announcement

---
+++ back-end/src/services/announcement/announcementCreateServices.ts
@@ -0,0 +1,32 @@
+import AppDataSource from "../../data-source";
+import { Announcement } from "../../entities/announcement.entity";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appError";
+import { ICreateAnnouncement } from "../../interfaces/announcement";
+
+const announcementCreateServices = async (
+  announcement: ICreateAnnouncement,
+  userId: string
+) => {
+  const announcementRepository = AppDataSource.getRepository(Announcement);
+  const userRepository = AppDataSource.getRepository(User);
+  const user = await userRepository.findOneBy({ id: userId });
+
+  if (!user) {
+    throw new AppError(404, "user not found");
+  }
+  const announcementCreate = announcementRepository.create(announcement);
+  announcementCreate.user = user;
+  await announcementRepository.save(announcementCreate);
+
+  const announcementCreated = await announcementRepository.findOneBy({
+    id: announcementCreate.id,
+  });
+  console.log(
+    "testestestexteteswasdfsdiofsdo9fhsauiodhfuiosadfnvuiosadfnguioaefhguioefhnguiosadfhguiopsadfhnguipsadbnjiofnsadjifnsaduiofnsaduifnsauidfhnuisadnf"
+  );
+
+  console.log(announcementCreated);
+  return announcementCreated;
+};
+export default announcementCreateServices;
