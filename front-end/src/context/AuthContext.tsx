de826e8aeee78adbf4e71a859a3eea7df1791989
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-03-07 19:07:06 -0300

    Feat: new functions and states

--- front-end/src/context/AuthContext.tsx
+++ front-end/src/context/AuthContext.tsx
@@ -1,7 +1,14 @@
-import { createContext, Dispatch, ReactNode, useEffect, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { FieldValues } from "react-hook-form";
 import { NavigateFunction, useNavigate } from "react-router-dom";
-import { IUser } from "./interfaces";
+import { IAnnouncement, IUser } from "./interfaces";
 import { AxiosError } from "axios";
 import api from "../services";
 
@@ -9,18 +16,24 @@ interface IAuthContext {
   requestLogin: (data: FieldValues) => Promise<void>;
   requestRegister: (data: FieldValues) => Promise<void>;
   requestCreateAnnouncement: (data: FieldValues) => Promise<void>;
+  requestUpdateAnnouncement: (data: FieldValues) => Promise<void>;
   requestDeleteAnnouncement: (id: string) => Promise<void>;
+  initialLetters: (names: string) => string;
   navigate: NavigateFunction;
   user: IUser | null | undefined;
+  announcementUser: IAnnouncement[];
+  setAnnouncementUser: Dispatch<SetStateAction<IAnnouncement[]>>;
+  announcementView: IAnnouncement | null;
+  setAnnouncementView: Dispatch<SetStateAction<IAnnouncement | null>>;
   isLoading: boolean;
   isOpenModalCreate: boolean;
-  setIsOpenModalCreate: Dispatch<React.SetStateAction<boolean>>;
+  setIsOpenModalCreate: Dispatch<SetStateAction<boolean>>;
   isOpenModalEdit: boolean;
-  setIsOpenModalEdit: Dispatch<React.SetStateAction<boolean>>;
+  setIsOpenModalEdit: Dispatch<SetStateAction<boolean>>;
   isOpenModalDelete: boolean;
-  setIsOpenModalDelete: Dispatch<React.SetStateAction<boolean>>;
-  productModal: any;
-  setProdutModal: Dispatch<React.SetStateAction<any>>;
+  setIsOpenModalDelete: Dispatch<SetStateAction<boolean>>;
+  productModal: IAnnouncement | null;
+  setProdutModal: Dispatch<SetStateAction<IAnnouncement | null>>;
 }
 
 interface IAuthProvider {
@@ -30,11 +43,14 @@ export const AuthContext = createContext({} as IAuthContext);
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
   const [user, setUser] = useState<IUser | null | undefined>();
+  const [announcementUser, setAnnouncementUser] = useState<IAnnouncement[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isOpenModalCreate, setIsOpenModalCreate] = useState<boolean>(false);
   const [isOpenModalEdit, setIsOpenModalEdit] = useState<boolean>(false);
   const [isOpenModalDelete, setIsOpenModalDelete] = useState<boolean>(false);
-  const [productModal, setProdutModal] = useState(null);
+  const [productModal, setProdutModal] = useState<IAnnouncement | null>(null);
+  const [announcementView, setAnnouncementView] =
+    useState<IAnnouncement | null>(null);
   const navigate = useNavigate();
 
   const requestLogin = async (data: FieldValues): Promise<void> => {
@@ -71,7 +87,7 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
     try {
       const res = await api.post("/user/announcement", data);
       setIsOpenModalCreate(false);
-      console.log(res.data);
+      setAnnouncementUser([res.data, ...announcementUser]);
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
         console.error(error.response?.data ?? "unknow error");
@@ -81,10 +97,36 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
     }
   };
 
+  const requestUpdateAnnouncement = async (data: FieldValues) => {
+    Object.keys(data).forEach((element) => {
+      if (data[element] === "" || data[element] === null) {
+        delete data[element];
+      }
+    });
+    try {
+      await api.patch(`/user/announcement/${productModal?.id}`, data);
+      const announcementIndex = announcementUser.findIndex(
+        (announcement) => announcement.id === productModal?.id
+      );
+      if (announcementIndex === -1) {
+        throw new Error("announcement id not found");
+      }
+      announcementUser[announcementIndex] = {
+        ...announcementUser[announcementIndex],
+        ...data,
+      };
+      setIsOpenModalEdit(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   const requestDeleteAnnouncement = async (id: string): Promise<void> => {
     try {
-      const res = await api.delete(`/user/announcement/${id}`);
-      console.log(res.data);
+      await api.delete(`/user/announcement/${id}`);
+      setAnnouncementUser(
+        announcementUser.filter((announcement) => announcement.id != id)
+      );
       setIsOpenModalDelete(false);
     } catch (error: unknown) {
       if (error instanceof AxiosError) {
@@ -95,6 +137,12 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
     }
   };
 
+  const initialLetters = (names: string) => {
+    let letters: string = "";
+    names.split(" ").forEach((name) => (letters += name[0]));
+    return letters;
+  };
+
   useEffect(() => {
     const requestProfile = async () => {
       const token = localStorage.getItem("@lojaDeCarros:token");
@@ -103,6 +151,7 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
           api.defaults.headers.common.authorization = `Bearer ${token}`;
           const { data } = await api.get("/user/profile");
           setUser(data);
+          setAnnouncementUser(data.announcement);
         } catch (error) {
           console.error(error);
         }
@@ -118,9 +167,12 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
         requestLogin,
         navigate,
         user,
+        announcementUser,
+        setAnnouncementUser,
         isLoading,
         requestRegister,
         requestCreateAnnouncement,
+        requestUpdateAnnouncement,
         requestDeleteAnnouncement,
         isOpenModalCreate,
         setIsOpenModalCreate,
@@ -130,6 +182,9 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
         setIsOpenModalDelete,
         productModal,
         setProdutModal,
+        announcementView,
+        setAnnouncementView,
+        initialLetters,
       }}
     >
       {children}
